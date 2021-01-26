95400f869cdfab6e66713e6357550ca336af27b7
simple925 <simple925@naver.com>
2021-01-26 16:31:59 +0900

    new부터해야됨

---
+++ index.js
@@ -0,0 +1,38 @@
+// index.js
+
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var app = express();
+
+// DB setting
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+mongoose.connect(process.env.MONGO_DB);
+var db = mongoose.connection;
+db.once('open', function(){
+  console.log('DB conneted');
+});
+db.on('error', function(){
+  console.log('DB ERROR : ', err);
+});
+
+// Other setting
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname+'/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(methodOverride('_method'));
+
+// Routes
+app.use('/', require('./routers/home'));
+app.use('/posts', require('./reuters/posts'));
+
+// Port setting
+var port = 3000;
+app.listen(port, function(){
+  console.log('server on! http://localhost:'+port);
+});
