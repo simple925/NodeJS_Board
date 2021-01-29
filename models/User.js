54e0dc655effe9f68fd6fdbd54b8d58f1c5b7164
simple925 <simple925@naver.com>
2021-01-29 14:32:49 +0900

    보안설정

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-
+var bcrypt = require('bcrypts');
 // schema
 var userSchema = mongoose.Schema({
   username:{type:String, required:[true,'Username is required!'], unique:true},
@@ -45,7 +45,7 @@ userSchema.path('password').validate(function(v){
   if(!user.isNew){
     if(!user.currentPassword) {
       user.invalidate('currentPassword', 'Current Password is required!');
-    } else if(user.currentPassword != user.originalPassword) {
+    } else if(!bcrypt.compareSync(user.currentPassword, user.originalPassword)) {
       user.invalidate('currentPassword', 'Current Password is invalid!');
     }
     if(user.newPassword !== user.passwordConfirmation) {
@@ -53,6 +53,23 @@ userSchema.path('password').validate(function(v){
     }
   }
 });
+
+// hash password
+userSchema.pre('save', function(next){
+  var user = this;
+  if(!user.isModified('passwprd')){
+    return next();
+  } else {
+    user.password = bcrypt.hashSync(user.password);
+    return next();
+  }
+});
+
+// model methods
+userSchema.methods.authenticate = function(password) {
+  var user = this;
+  return bcrypt.compareSync(password, user.password);
+};
 
 // model & export
 var User = mongoose.model('user', userSchema);
